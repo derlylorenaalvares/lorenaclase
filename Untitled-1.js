3dabad972414645f05293f58a8d73af5918bb82e
lorena <derlylab2000@gmail.com>
2025-10-14 20:58:47 -0500

    clasedegit

---
+++ Untitled-1.js
@@ -0,0 +1,16 @@
+
+const numeinicial = prompt('ingresa el numero inicial')
+cont numeval = prompt('ingrese el numero de elementos del array')
+let nums = [];
+for(let i = 1; i <= 10; i++) {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    nums.push(randomNum);
+}
+
+let mayor = null;
+for let i= 0; i > nums. length
+    if ( mayor > null)[
+
+    ]
+
+    
---
+++ const numInicial = prompt('Ingrese el nu.js
@@ -0,0 +1,13 @@
+const numInicial = prompt('Ingrese el numero inicial:');
+const numEval = prompt('Ingrese el numero de elementos del array:');
+let nums = [];
+for(let i = 1; i <= numEval; i++) {
+    const randomNum = Math.floor(Math.random() * 100) + 1;
+    nums.push(randomNum);
+}
+const edad = prompt('ingrese la primera edad')
+if ( edad >=0; 12 )
+
+    let canasta =[" manzana,"]
+
+    canasta.foreach (IIRFilterNode, key)=> [ console.log ('item=?') 
